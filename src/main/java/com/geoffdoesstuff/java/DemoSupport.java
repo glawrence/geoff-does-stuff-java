d22f8b0836c8dea5001a4e2a89c30e444112a631
Geoff Lawrence <3411808-glawrence@users.noreply.gitlab.com>
2021-02-28 16:22:59 +0000

    Add code to implement menu and easy running of demos

---
+++ src/main/java/com/geoffdoesstuff/java/DemoSupport.java
@@ -0,0 +1,130 @@
+package com.geoffdoesstuff.java;
+
+import java.io.File;
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.List;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
+import java.util.stream.Collectors;
+
+/**
+ * This class supports my "demos", which can be run independently or from the MainDemoApp menu. The code in this class
+ * uses reflection to find and execute any demo classes it finds.
+ */
+public final class DemoSupport {
+
+	/**
+	 * Hide the constructor as this is a class of static methods.
+	 */
+	private DemoSupport() {
+	}
+
+	/**
+	 * Based on the specified Java package name, we get the "resources" for this package from the class loader and then
+	 * seek all the classnames, to return a list of strings.
+	 * @param demoClasspath
+	 * @return all matching class names as a List of Strings, sorted alphabetically
+	 */
+	public static List<String> getDemoClasses(String demoClasspath) {
+		final String demoClassPathSlash = demoClasspath.replace('.', '/');
+
+		List<String> classNames = new ArrayList<>();
+		try {
+			ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+			Enumeration<URL> resources = classLoader.getResources(demoClassPathSlash);
+			while (resources.hasMoreElements()) {
+				URL resource = resources.nextElement();
+				switch (resource.getProtocol()) {
+					case "file":
+						// classes are being loaded from .class files on the file system
+						classNames.addAll(findClassesInDirectory(resource.getFile(), demoClasspath));
+						break;
+					case "jar":
+						// classes have been packaged into a JAR file, so we need to parse it
+						classNames.addAll(findClassesInJarFile(resource.getFile().split(MainDemoApp.REGEX)[1], demoClassPathSlash));
+						break;
+					default:
+						// was not expecting to be here, but if we are, output some information
+						System.out.println("Unexpected class loader resource protocol - " + resource.getFile());
+						System.out.println("Resource: " + resource.getClass().getName() + " - " + resource.toString());
+						System.out.println("  " + resource.getProtocol());
+						System.out.println("  " + resource.getPath());
+						System.out.println("  " + resource.getFile().getClass().getName());
+						break;
+				}
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return classNames.stream().sorted().collect(Collectors.toList());
+	}
+
+	/**
+	 * First we get the Class object for the specified String className. Then we search for and hopefully find the
+	 * "public static void main(String[] args)" method, which we can then just execute.
+	 * @param className
+	 */
+	public static void executeDemoClass(String className) {
+		try {
+			Class<?> theClass = Class.forName(className);
+			System.out.println("Class: " + theClass.getName());
+			// the main method is static, so we don't need to instantiate the class and make an object
+			Method main = theClass.getDeclaredMethod("main", String[].class);
+			Object[] params = new Object[1];
+			main.invoke(null, params);
+		} catch (NoSuchMethodException | ClassNotFoundException | IllegalAccessException | InvocationTargetException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private static List<String> findClassesInDirectory(String dirName, String packageName) {
+		List<String> classes = new ArrayList<>();
+		File directory = new File(dirName);
+		if (!directory.exists()) {
+			return classes;
+		}
+		File[] files = directory.listFiles();
+		if (files == null) {
+			return classes;
+		}
+		for (File file : files) {
+			if (file.isDirectory()) {
+				// this is a recursive call
+				classes.addAll(findClassesInDirectory(file.getName(), packageName + "." + file.getName()));
+			} else if (file.getName().endsWith(".class") && !file.getName().contains("$")) {
+				// here we have a class file but without a $ in the name, which usually indicates an inner class
+				classes.add(packageName + "." + file.getName().substring(0, file.getName().length() - 6));
+			}
+		}
+		return classes;
+	}
+
+	private static List<String> findClassesInJarFile(String jarName, String packageName) {
+		List<String> classes = new ArrayList<>();
+		try {
+			JarFile jar = new JarFile(jarName);
+			Enumeration<JarEntry> enumOfJar = jar.entries();
+			while (enumOfJar.hasMoreElements()) {
+				JarEntry jarEntry = enumOfJar.nextElement();
+				if (!jarEntry.isDirectory()) {
+					String className = jarEntry.getName();
+					if (className.startsWith(packageName)) {
+						if (!className.contains("$")) {
+							className = className.substring(0, className.length() - 6); //remove ".class" from the end
+							className = className.replace('/', '.'); //change / characters to .
+							classes.add(className);
+						}
+					}
+				}
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return classes;
+	}
+}
---
+++ src/main/java/com/geoffdoesstuff/java/MainDemoApp.java
@@ -0,0 +1,45 @@
+package com.geoffdoesstuff.java;
+
+import java.util.*;
+
+public class MainDemoApp {
+
+	public static final String REGEX = "[:!]"; // match : or !
+	private static final String DEMO_CLASSPATH = "com.geoffdoesstuff.java.demo";
+
+	public static void main(String[] args) {
+		System.out.println("Welcome....");
+		List<String> classNames = DemoSupport.getDemoClasses(DEMO_CLASSPATH);
+		System.out.println("Demo classes found: " + classNames.size());
+		Scanner keyboard = new Scanner(System.in);
+		displayMenu(classNames);
+		do {
+			String input = keyboard.nextLine();
+			if (input != null) {
+				int inputIndex;
+				if (input.length() == 1) {
+					inputIndex = input.toUpperCase().charAt(0) - 65;
+					if ((inputIndex >= 0) && (inputIndex < classNames.size())) {
+						DemoSupport.executeDemoClass(classNames.get(inputIndex));
+					}
+				}
+				if ("EX".equalsIgnoreCase(input)) {
+					break;
+				}
+			}
+			displayMenu(classNames);
+		} while (true);
+		keyboard.close();
+		System.out.println("All done!");
+	}
+
+	private static void displayMenu(List<String> classNames) {
+		System.out.println("Options:");
+		for (int i = 0; i < classNames.size(); i++) {
+			System.out.println("  " + Character.toString(65 + i) + ") " + classNames.get(i));
+		}
+		System.out.println("  ex) exit");
+		System.out.println("Enter command:");
+	}
+
+}

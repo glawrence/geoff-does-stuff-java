1ba3dafd82915a5c363fc276eb94307667853abc
Geoff Lawrence <3411808-glawrence@users.noreply.gitlab.com>
2021-03-01 07:22:26 +0000

    Added UUID demo

---
+++ src/main/java/com/geoffdoesstuff/java/demo/UniqueID.java
@@ -0,0 +1,26 @@
+package com.geoffdoesstuff.java.demo;
+
+import java.util.UUID;
+
+/**
+ * In Java a UUID is an immutable universally unique ID representing a 128bit value, which is made from two Long numbers
+ * which are 64-bit.
+ */
+public class UniqueID {
+	public static void main(String[] args) {
+		UUID uuid = UUID.randomUUID();
+		System.out.println("Random UUID: " + uuid);
+		System.out.println(String.format("  Most Significant Bits: %d Least Significant Bits: %d", uuid.getMostSignificantBits(), uuid.getLeastSignificantBits()));
+		System.out.println(String.format("  Version: %d Variant: %d", uuid.version(), uuid.variant()));
+
+		uuid = new UUID(123456789, 987654321);
+		System.out.println("Generated UUID: " + uuid);
+		System.out.println(String.format("  Most Significant Bits: %d Least Significant Bits: %d", uuid.getMostSignificantBits(), uuid.getLeastSignificantBits()));
+		System.out.println(String.format("  Version: %d Variant: %d", uuid.version(), uuid.variant()));
+
+		uuid = new UUID(1, -1);
+		System.out.println("Generated UUID: " + uuid);
+		System.out.println(String.format("  Most Significant Bits: %d Least Significant Bits: %d", uuid.getMostSignificantBits(), uuid.getLeastSignificantBits()));
+		System.out.println(String.format("  Version: %d Variant: %d", uuid.version(), uuid.variant()));
+	}
+}

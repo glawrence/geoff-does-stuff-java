f7d1bf5785fb23c75108df17f0df574364d1c0d1
Geoff Lawrence <3411808-glawrence@users.noreply.gitlab.com>
2021-03-15 15:38:35 +0000

    Improved JavaSystemInfo class

--- src/main/java/com/geoffdoesstuff/java/utility/JavaSystemInfo.java
+++ src/main/java/com/geoffdoesstuff/java/utility/JavaSystemInfo.java
@@ -21,6 +21,38 @@ public class JavaSystemInfo {
 		System.out.println(getPropertyOutput("java.home"));
 	}
 
+	/**
+	 * Check if we are on Java 8 or higher, returning true if we are
+	 * @return boolean
+	 */
+	public static boolean isAtLeastJava8() {
+		return (System.getProperty("java.version", "0.0").compareTo("1.8") >= 0);
+	}
+
+	/**
+	 * Check if we are on Java 11 or higher, returning true if we are
+	 * @return boolean
+	 */
+	public static boolean isAtLeastJava11() {
+		return (System.getProperty("java.version", "0.0").compareTo("11") >= 0);
+	}
+
+	/**
+	 * Check if we are on Java 15 or higher, returning true if we are
+	 * @return boolean
+	 */
+	public static boolean isAtLeastJava15() {
+		return (System.getProperty("java.version", "0.0").compareTo("15") >= 0);
+	}
+
+	/**
+	 * Check if we are on Java 16 or higher, returning true if we are
+	 * @return boolean
+	 */
+	public static boolean isAtLeastJava16() {
+		return (System.getProperty("java.version", "0.0").compareTo("16") >= 0);
+	}
+
 	private static String getPropertyOutput(String strProperty) {
 		return strProperty.replaceAll("[.]", " ") + ": " + System.getProperty(strProperty, strProperty + " was not found");
 	}
---
+++ src/test/java/com/geoffdoesstuff/java/utility/JavaSystemInfoTest.java
@@ -0,0 +1,50 @@
+package com.geoffdoesstuff.java.utility;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class JavaSystemInfoTest {
+
+	@Test
+	@DisplayName("Just make sure outputJavaInfo() does not throw an exception")
+	void outputJavaInfoTest() {
+		// using a method reference and asserting nothing goes wrong
+		assertDoesNotThrow(JavaSystemInfo::outputJavaInfo);
+	}
+
+	@Test
+	@DisplayName("Test the actual isAtLeastJavaXX() code")
+	void isAtLeastJavaXXTest() {
+		assertTrue(JavaSystemInfo.isAtLeastJava8());
+		assertTrue(JavaSystemInfo.isAtLeastJava11());
+		assertTrue(JavaSystemInfo.isAtLeastJava15());
+		assertFalse(JavaSystemInfo.isAtLeastJava16());
+	}
+
+	@Test
+	@DisplayName("Test the way isAtLeastJavaXX() works")
+	void isAtLeastJava11Test() {
+		final String ELEVEN = "11";
+		String javaVersion;
+
+		javaVersion = "1.8.0_202"; //this is Java 8
+		assertFalse(javaVersion.compareTo(ELEVEN) >= 0);
+		javaVersion = "10.1.7";
+		assertFalse(javaVersion.compareTo(ELEVEN) >= 0);
+
+		javaVersion = "11";
+		assertTrue(javaVersion.compareTo(ELEVEN) >= 0);
+		javaVersion = "11.0";
+		assertTrue(javaVersion.compareTo(ELEVEN) >= 0);
+		javaVersion = "11.0.1";
+		assertTrue(javaVersion.compareTo(ELEVEN) >= 0);
+		javaVersion = "15.0.2";
+		assertTrue(javaVersion.compareTo(ELEVEN) >= 0);
+		javaVersion = "15.0";
+		assertTrue(javaVersion.compareTo(ELEVEN) >= 0);
+		javaVersion = "16.0";
+		assertTrue(javaVersion.compareTo(ELEVEN) >= 0);
+	}
+}

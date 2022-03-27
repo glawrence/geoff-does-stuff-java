72932887040a2ecee021a49930f157bdb16d824d
Geoff Lawrence <3411808-glawrence@users.noreply.gitlab.com>
2022-03-27 21:52:29 +0100

    Comment converted to JavaDoc

--- src/main/java/com/geoffdoesstuff/java/demo/optionals/PopulateOptionals.java
+++ src/main/java/com/geoffdoesstuff/java/demo/optionals/PopulateOptionals.java
@@ -3,10 +3,16 @@ package com.geoffdoesstuff.java.demo.optionals;
 import java.util.Optional;
 import java.util.concurrent.ThreadLocalRandom;
 
+/**
+ * Utility class helping to populate Optional.
+ */
 public class PopulateOptionals {
-    /*
-        This method takes an object and randomly returns the optional with the object in or an empty optional.
-        This should mean the code using this then does not show warnings that the test is always true or false.
+    /**
+     * This method takes an object and randomly returns the optional with the object in or an empty optional.
+     * This should mean the code using this then does not show warnings that the test is always true or false.
+     * @param object the object that is "optionally" returned
+     * @param <T> the object type
+     * @return and Optional, that is randomly populated or not
      */
     public static <T> Optional<T> randomlyPopulate(T object) {
         ThreadLocalRandom random = ThreadLocalRandom.current();

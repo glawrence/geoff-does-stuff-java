c49f3c3b0357cbacf5a6f2affacfa6e6fa1695b1
Geoff Lawrence <3411808-glawrence@users.noreply.gitlab.com>
2023-08-19 19:58:27 +0100

    Upgrade to Gradle & JUnit

---
+++ src/test/java/com/geoffdoesstuff/java/utility/DemoUtilitiesTest.java
@@ -0,0 +1,20 @@
+package com.geoffdoesstuff.java.utility;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DemoUtilitiesTest {
+
+    @Test
+    @Disabled
+    void outputTitle() {
+    }
+
+    @Test
+    @Disabled
+    void testOutputTitle() {
+        assertTrue(false);
+    }
+}

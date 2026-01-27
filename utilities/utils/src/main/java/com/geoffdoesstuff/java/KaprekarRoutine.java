438d550bf55a4e8bc7cca1545d6227a2cc9ec0f9
Geoff Lawrence <3411808-glawrence@users.noreply.gitlab.com>
2026-01-27 21:17:35 +0000

    File format updates

--- utilities/utils/src/main/java/com/geoffdoesstuff/java/KaprekarRoutine.java
+++ utilities/utils/src/main/java/com/geoffdoesstuff/java/KaprekarRoutine.java
@@ -28,121 +28,121 @@ import java.util.Scanner;
  */
 public class KaprekarRoutine {
 
-    /**
-     * This is here to suppress JavaDoc complaining about not commenting the default constructor
-     */
-    private KaprekarRoutine() {
-    }
+	/**
+	 * This is here to suppress JavaDoc complaining about not commenting the default constructor
+	 */
+	private KaprekarRoutine() {
+	}
 
-    /**
-     * Start the Kaprekar Routine utility, which can be run in test mode, demo mode, or interactive mode, where the
-     * latter is the default
-     * @param args only first arg is used to specify mode (test or demo)
-     */
-    public static void main(String[] args) {
-        Map<String, String> arguments = CommandLine.processArgs(args, true);
+	/**
+	 * Start the Kaprekar Routine utility, which can be run in test mode, demo mode, or interactive mode, where the
+	 * latter is the default
+	 * @param args only first arg is used to specify mode (test or demo)
+	 */
+	public static void main(String[] args) {
+		Map<String, String> arguments = CommandLine.processArgs(args, true);
 
-        if (arguments.isEmpty()) {
-            Scanner keyboard = new Scanner(System.in);
-            do {
-                displayMenu();
-                String input = keyboard.nextLine();
-                if (input != null) {
-                    if ("EX".equalsIgnoreCase(input)) {
-                        break;
-                    }
-                    process(input);
-                }
-            } while (true);
-            keyboard.close();
-        } else {
-            if ((arguments.size() == 1) && arguments.containsKey("test")) {
-                process(null);
-                process("");
-                process("123");
-                process("12345");
-                process("aaaa");
-                process("0001");
-                process("1111");
-                process("3524");
-            } else if ((arguments.size() == 1) && arguments.containsKey("demo")) {
-                process("0213");
-                process("0911");
-                process("0909");
-                process("0990");
-                process("9009");
-                process("9090");
-                process("9900");
-                process("9991");
-                process("2221");
-                process("7648");
-                process("2413");
-            } else {
-                System.err.println("Unknown arguments found. " + CommandLine.getFormattedArguments(arguments));
-                System.out.println("Please use \"test\" or \"demo\", alternatively don't specify any argument for interactive mode.");
-            }
-        }
-    }
+		if (arguments.isEmpty()) {
+			Scanner keyboard = new Scanner(System.in);
+			do {
+				displayMenu();
+				String input = keyboard.nextLine();
+				if (input != null) {
+					if ("EX".equalsIgnoreCase(input)) {
+						break;
+					}
+					process(input);
+				}
+			} while (true);
+			keyboard.close();
+		} else {
+			if ((arguments.size() == 1) && arguments.containsKey("test")) {
+				process(null);
+				process("");
+				process("123");
+				process("12345");
+				process("aaaa");
+				process("0001");
+				process("1111");
+				process("3524");
+			} else if ((arguments.size() == 1) && arguments.containsKey("demo")) {
+				process("0213");
+				process("0911");
+				process("0909");
+				process("0990");
+				process("9009");
+				process("9090");
+				process("9900");
+				process("9991");
+				process("2221");
+				process("7648");
+				process("2413");
+			} else {
+				System.err.println("Unknown arguments found. " + CommandLine.getFormattedArguments(arguments));
+				System.out.println("Please use \"test\" or \"demo\", alternatively don't specify any argument for interactive mode.");
+			}
+		}
+	}
 
-    private static void displayMenu() {
-        System.out.println("Enter number or EX to quit");
-    }
+	private static void displayMenu() {
+		System.out.println("Enter number or EX to quit");
+	}
 
-    private static void process(String number) {
-        boolean valid = valid(number);
-        if (valid) {
-            System.out.println("Number " + number + " is valid");
-            String nextNumber = number;
-            int i = 0;
-            while (!nextNumber.equals("6174")) {
-                nextNumber = processToNewNumber(nextNumber);
-                if (++i == 30) throw new IllegalArgumentException("Invalid input, leading to infinite loop - " + number);
-            }
-        } else {
-            System.out.println("Number " + number + " is not valid");
-        }
-    }
+	private static void process(String number) {
+		boolean valid = valid(number);
+		if (valid) {
+			System.out.println("Number " + number + " is valid");
+			String nextNumber = number;
+			int i = 0;
+			while (!nextNumber.equals("6174")) {
+				nextNumber = processToNewNumber(nextNumber);
+				if (++i == 30) throw new IllegalArgumentException("Invalid input, leading to infinite loop - " + number);
+			}
+		} else {
+			System.out.println("Number " + number + " is not valid");
+		}
+	}
 
-    private static boolean valid(String number) {
-        boolean valid;
-        if (Objects.isNull(number)) {
-            valid = false;
-        } else if (number.length() != 4) {
-            valid = false;
-        } else {
-            valid = true;
-        }
-        if (valid) {
-            try {
-                int integer = Integer.parseInt(number);
-                char[] digits = String.format("%04d", integer).toCharArray();
-                if ((digits[0] == digits[1]) && (digits[1] == digits[2]) && (digits[2] == digits[3])) {
-                    valid = false;
-                }
-            } catch (NumberFormatException e) {
-                valid = false;
-            }
-        }
-        return valid;
-    }
+	private static boolean valid(String number) {
+		boolean valid;
+		if (Objects.isNull(number)) {
+			valid = false;
+		} else if (number.length() != 4) {
+			valid = false;
+		} else {
+			valid = true;
+		}
+		if (valid) {
+			try {
+				int integer = Integer.parseInt(number);
+				char[] digits = String.format("%04d", integer).toCharArray();
+				if ((digits[0] == digits[1]) && (digits[1] == digits[2]) && (digits[2] == digits[3])) {
+					valid = false;
+				}
+			} catch (NumberFormatException e) {
+				valid = false;
+			}
+		}
+		return valid;
+	}
 
-    private static String largest(String number) {
-        char[] digits = number.toCharArray();
-        Arrays.sort(digits);
-        return String.valueOf(digits[3]) + digits[2] + digits[1] + digits[0];
-    }
+	private static String largest(String number) {
+		char[] digits = number.toCharArray();
+		Arrays.sort(digits);
+		return String.valueOf(digits[3]) + digits[2] + digits[1] + digits[0];
+	}
 
-    private static String smallest(String number) {
-        char[] digits = number.toCharArray();
-        Arrays.sort(digits);
-        return String.valueOf(digits[0]) + digits[1] + digits[2] + digits[3];
-    }
+	private static String smallest(String number) {
+		char[] digits = number.toCharArray();
+		Arrays.sort(digits);
+		return String.valueOf(digits[0]) + digits[1] + digits[2] + digits[3];
+	}
 
-    private static String processToNewNumber(String number) {
-        int smallest = Integer.parseInt(smallest(number));
-        int largest = Integer.parseInt(largest(number));
-        String difference = String.format("%04d", largest - smallest);
-        System.out.println("  Number " + number + " becomes " + largest(number) + " and " + smallest(number) + " giving " + difference);
-        return difference;
-    }
+	private static String processToNewNumber(String number) {
+		int smallest = Integer.parseInt(smallest(number));
+		int largest = Integer.parseInt(largest(number));
+		String difference = String.format("%04d", largest - smallest);
+		System.out.println("  Number " + number + " becomes " + largest(number) + " and " + smallest(number) + " giving " + difference);
+		return difference;
+	}
 }

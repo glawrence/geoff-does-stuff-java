3fc04b37f7f2f8eaf979bed9ae65c420cf4af477
Geoff Lawrence <3411808-glawrence@users.noreply.gitlab.com>
2026-03-17 15:55:57 +0000

    Adding extra utility scripts

---
+++ utilities/scripts/convert.cmd
@@ -0,0 +1,23 @@
+@ECHO OFF
+SETLOCAL
+
+SET REPO_BASE=C:\git\geoff-does-stuff-java
+
+SET LIBRARY_JAR=%REPO_BASE%\utilities\library\build\libs\library.jar
+SET UTILS_JAR=%REPO_BASE%\utilities\utils\build\libs\utils.jar
+SET CLASSPATH=%LIBRARY_JAR%;%UTILS_JAR%
+
+IF NOT EXIST %LIBRARY_JAR% (
+	ECHO Build missing, building now...
+	CALL "%REPO_BASE%\gradlew" -p "%REPO_BASE%" build
+)
+IF NOT EXIST %UTILS_JAR% (
+	ECHO Build missing, building now...
+	CALL "%REPO_BASE%\gradlew" -p "%REPO_BASE%" build
+)
+
+SET /P MS_TIMESTAMP="Please enter MS Timestamp: "
+
+"%JAVA_HOME%\bin\java" -Dfile.encoding=UTF-8 -classpath %CLASSPATH% com.geoffdoesstuff.java.Conversions --ms-timestamp=%MS_TIMESTAMP%
+
+ENDLOCAL
---
+++ utilities/scripts/convert.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+REPO_BASE=~/git/geoff-does-stuff-java
+
+LIBRARY_JAR=$REPO_BASE/utilities/library/build/libs/library.jar
+UTILS_JAR=$REPO_BASE/utilities/utils/build/libs/utils.jar
+CLASSPATH=$LIBRARY_JAR:$UTILS_JAR
+
+if [[ ! -f "$LIBRARY_JAR" ]] || [[ ! -f "$UTILS_JAR" ]]; then
+  echo "Build missing, building now..."
+  $REPO_BASE/gradlew -p $REPO_BASE build
+fi
+
+read -r -p 'Please enter MS Timestamp: ' MS_TIMESTAMP
+
+$JAVA_HOME/bin/java -Dfile.encoding=UTF-8 -classpath $CLASSPATH com.geoffdoesstuff.java.Conversions --ms-timestamp=$MS_TIMESTAMP
